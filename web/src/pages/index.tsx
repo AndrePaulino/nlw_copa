36da835ec4e87170bf546b10cf20ff8e4ee09999
Andre Paulino <andrepaulinobtw@gmail.com>
2022-11-02 15:16:56 -0300

    add postcss, autoprefixer and setup tailwindcss

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -3,14 +3,12 @@ interface HomeProps {
 }
 
 export default function Home(props: HomeProps) {
-	return <h1>Pools count: {props.count}</h1>;
+	return <h1 className="text-violet-600 font-bold text-xl">Bolões: {props.count}</h1>;
 }
 
 export const getServerSideProps = async () => {
 	const response = await fetch("http://localhost:3333/pools/count");
 	const data = await response.json();
-
-	console.log(data);
 
 	return {
 		props: {
