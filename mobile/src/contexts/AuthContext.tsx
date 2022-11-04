089dbf6b0f8e8df47e35a77f3c889f8fa2063021
Andre Paulino <andrepaulinobtw@gmail.com>
2022-11-03 21:55:22 -0300

    added google OAuth

--- mobile/src/contexts/AuthContext.tsx
+++ mobile/src/contexts/AuthContext.tsx
@@ -1,4 +1,9 @@
-import React, { createContext, ReactNode } from "react";
+import React, { createContext, ReactNode, useState, useEffect } from "react";
+import * as AuthSession from "expo-auth-session";
+import * as Google from "expo-auth-session/providers/google";
+import * as WebBrowser from "expo-web-browser";
+
+WebBrowser.maybeCompleteAuthSession();
 
 interface UserProps {
 	name: string;
@@ -11,24 +16,50 @@ interface AuthContextProviderProps {
 
 export interface AuthContextDataProps {
 	user: UserProps;
+	isUserLoading: boolean;
 	signIn: () => Promise<void>;
 }
 
 export const AuthContext = createContext({} as AuthContextDataProps);
 
 export function AuthContextProvider({ children }: AuthContextProviderProps) {
+	const [user, setUser] = useState<UserProps>({} as UserProps);
+	const [isUserLoading, setIsUserLoading] = useState(false);
+
+	const [request, response, promptAsync] = Google.useAuthRequest({
+		clientId:
+			"495403270272-25ibag4435n9dtj9dpuoveom24la4a0t.apps.googleusercontent.com",
+		redirectUri: AuthSession.makeRedirectUri({ useProxy: true }),
+		scopes: ["profile", "email"],
+	});
+
 	async function signIn() {
-		console.log("Vamos logar.");
+		try {
+			setIsUserLoading(true);
+			await promptAsync();
+		} catch (err) {
+			console.log(err);
+			throw err;
+		} finally {
+			setIsUserLoading(false);
+		}
 	}
+
+	async function signInWithGoogle(accessToken: string) {
+		console.log("ACCESS_TOKEN => ", accessToken);
+	}
+
+	useEffect(() => {
+		if (response?.type == "success" && response.authentication?.accessToken)
+			signInWithGoogle(response.authentication.accessToken);
+	}, [response]);
 
 	return (
 		<AuthContext.Provider
 			value={{
+				user,
+				isUserLoading,
 				signIn,
-				user: {
-					name: "UsuarioTest",
-					avatarUrl: "https://github.com/identicons/avatarUrl?.png",
-				},
 			}}
 		>
 			{children}
