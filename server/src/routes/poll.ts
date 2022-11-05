3dca26517ac6f72835fc32170bea79d0c76340aa
Andre Paulino <andrepaulinobtw@gmail.com>
2022-11-04 21:16:25 -0300

    Added poll join route

--- server/src/routes/poll.ts
+++ server/src/routes/poll.ts
@@ -3,6 +3,7 @@ import { z } from 'zod'
 import ShortUniqueId from 'short-unique-id'
 
 import { prisma } from "../lib/prisma"
+import { authenticate } from "../plugins/authenticate"
 
 export async function pollRoutes(fastify: FastifyInstance) {
 	fastify.get('/polls/count', async () => {
@@ -48,4 +49,55 @@ export async function pollRoutes(fastify: FastifyInstance) {
 
 		return reply.status(201).send({ code })
 	})
+
+	fastify.post('/polls/:id/join', { onRequest: [authenticate] }, async (request, reply) => {
+		const joinPollBody = z.object({
+			code: z.string()
+		})
+
+		const { code } = joinPollBody.parse(request.body)
+
+		const poll = await prisma.poll.findUnique({
+			where: {
+				code,
+			},
+			include: {
+				participants: {
+					where: {
+						userId: request.user.sub
+					}
+				}
+			}
+		})
+
+		if (!poll) {
+			return reply.status(400).send({ message: "Bolão não encontrado." })
+		}
+
+		if (poll.participants.length > 0) {
+			return reply.status(400).send({ message: "Você já participa deste bolão." })
+
+		}
+
+		if (!poll.ownerId) {
+			await prisma.poll.update({
+				where: {
+					id: poll.id
+				},
+				data: {
+					ownerId: request.user.sub
+				}
+			})
+		}
+
+		await prisma.participant.create({
+			data: {
+				pollId: poll.id,
+				userId: request.user.sub,
+			}
+		})
+
+		return reply.status(201).send()
+	})
+
 }
