479eaba7188b72a6b86a5a3a208debf6f54a95b6
Andre Paulino <andrepaulinobtw@gmail.com>
2022-11-08 00:16:47 -0300

    Connected Polls screen to backend

--- mobile/src/screens/Polls.tsx
+++ mobile/src/screens/Polls.tsx
@@ -1,12 +1,44 @@
-import { VStack, Text, Icon } from "native-base";
+import { VStack, Icon, useToast, FlatList } from "native-base";
+import { useState, useCallback } from "react";
 import { FontAwesome5 } from "@expo/vector-icons";
-import { useNavigation } from "@react-navigation/native";
+import { useNavigation, useFocusEffect } from "@react-navigation/native";
+import { api } from "../services/api";
 
 import { Button } from "../components/Button";
 import { Header } from "../components/Header";
+import { Loading } from "../components/Loading";
+import { EmptyPollList } from "../components/EmptyPollList";
+import { PollCard, PollCardProps } from "../components/PollCard";
 
 export function Polls() {
 	const { navigate } = useNavigation();
+	const toast = useToast();
+
+	const [isLoading, setIsLoading] = useState(true);
+	const [polls, setPolls] = useState<PollCardProps[]>([]);
+
+	async function fetchPolls() {
+		try {
+			setIsLoading(true);
+			const pollsResponse = await api.get("/polls");
+			setPolls(pollsResponse.data.polls);
+		} catch (error) {
+			console.log(error);
+			toast.show({
+				title: "Não foi possível carregar os bolões.",
+				placement: "top",
+				bgColor: "red.500",
+			});
+		} finally {
+			setIsLoading(false);
+		}
+	}
+
+	useFocusEffect(
+		useCallback(() => {
+			fetchPolls();
+		}, [])
+	);
 
 	return (
 		<VStack flex={1} bgColor={"gray.900"}>
@@ -33,6 +65,20 @@ export function Polls() {
 					}
 				/>
 			</VStack>
+
+			{isLoading ? (
+				<Loading />
+			) : (
+				<FlatList
+					data={polls}
+					keyExtractor={(item) => item.id}
+					renderItem={({ item }) => <PollCard data={item} />}
+					ListEmptyComponent={() => <EmptyPollList />}
+					px={5}
+					showsVerticalScrollIndicator={false}
+					_contentContainerStyle={{ pb: 10 }}
+				/>
+			)}
 		</VStack>
 	);
 }
