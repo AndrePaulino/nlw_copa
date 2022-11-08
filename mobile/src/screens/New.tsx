5a7b09ff6e50b6936b062fad559750518983e225
Andre Paulino <andrepaulinobtw@gmail.com>
2022-11-07 23:09:40 -0300

    Connect New screen to backend

--- mobile/src/screens/New.tsx
+++ mobile/src/screens/New.tsx
@@ -1,11 +1,48 @@
-import { Heading, Text, VStack } from "native-base";
+import { useState } from "react";
+import { Heading, Text, VStack, useToast } from "native-base";
 import Logo from "../assets/logo.svg";
+import { api } from "../services/api";
 
 import { Header } from "../components/Header";
 import { Input } from "../components/Input";
 import { Button } from "../components/Button";
 
 export function New() {
+	const [title, setTitle] = useState("");
+	const [isLoading, setIsLoading] = useState(false);
+	const toast = useToast();
+
+	async function handlePollCreate() {
+		if (!title.trim()) {
+			return toast.show({
+				title: "Informe um nome para seu bolão.",
+				placement: "top",
+				bgColor: "red.500",
+			});
+		}
+
+		try {
+			setIsLoading(true);
+			await api.post("/polls", { title });
+
+			toast.show({
+				title: "Não foi possível criar o bolão.",
+				placement: "top",
+				bgColor: "green.500",
+			});
+			setTitle("");
+		} catch (error) {
+			console.log(error);
+			toast.show({
+				title: "Não foi possível criar o bolão.",
+				placement: "top",
+				bgColor: "red.500",
+			});
+		} finally {
+			setIsLoading(false);
+		}
+	}
+
 	return (
 		<VStack flex={1} bgColor={"gray.900"}>
 			<Header title={"Criar novo bolão"} />
@@ -21,8 +58,17 @@ export function New() {
 					Crie seu próprio bolão da copa e compartilhe entre amigos!
 				</Heading>
 
-				<Input mb={4} placeholder={"Qual nome do seu bolão?"} />
-				<Button title={"Criar meu bolão"} />
+				<Input
+					mb={4}
+					placeholder={"Qual nome do seu bolão?"}
+					onChangeText={setTitle}
+					value={title}
+				/>
+				<Button
+					title={"Criar meu bolão"}
+					onPress={handlePollCreate}
+					isLoading={isLoading}
+				/>
 				<Text
 					color={"gray.200"}
 					fontSize={"sm"}
