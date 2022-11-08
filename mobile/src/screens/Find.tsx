ba0e8d9b1e377a814b3fca9301878eec968f884c
Andre Paulino <andrepaulinobtw@gmail.com>
2022-11-08 01:17:26 -0300

    Connected Find screen to backend

--- mobile/src/screens/Find.tsx
+++ mobile/src/screens/Find.tsx
@@ -1,10 +1,70 @@
-import { Heading, VStack } from "native-base";
+import { useState } from "react";
+import { Heading, VStack, useToast } from "native-base";
+import { useNavigation } from "@react-navigation/native";
+import { api } from "../services/api";
 
 import { Header } from "../components/Header";
 import { Input } from "../components/Input";
 import { Button } from "../components/Button";
 
 export function Find() {
+	const { navigate } = useNavigation();
+	const toast = useToast();
+
+	const [isLoading, setIsLoading] = useState(false);
+	const [code, setCode] = useState("");
+
+	async function handleJoinPoll() {
+		try {
+			setIsLoading(true);
+
+			if (!code.trim()) {
+				return toast.show({
+					title: "Informe o código.",
+					placement: "top",
+					bgColor: "red.500",
+				});
+			}
+
+			await api.post("/polls/join", { code });
+
+			toast.show({
+				title: "Entrou no bolão com sucesso.",
+				placement: "top",
+				bgColor: "green.500",
+			});
+
+			navigate("polls");
+		} catch (error) {
+			console.log(error);
+			setIsLoading(false);
+
+			if (error.response?.data?.message === "Bolão não encontrado.") {
+				return toast.show({
+					title: "Bolão não encontrado.",
+					placement: "top",
+					bgColor: "red.500",
+				});
+			}
+			if (
+				error.response?.data?.message ===
+				"Você já participa deste bolão."
+			) {
+				return toast.show({
+					title: "Você já esta neste bolão.",
+					placement: "top",
+					bgColor: "red.500",
+				});
+			}
+
+			return toast.show({
+				title: "Não foi possível encontrar o bolão.",
+				placement: "top",
+				bgColor: "red.500",
+			});
+		}
+	}
+
 	return (
 		<VStack flex={1} bgColor={"gray.900"}>
 			<Header title={"Buscar por código"} showBackButton />
@@ -19,8 +79,17 @@ export function Find() {
 					Encontre um bolão através de seu código único
 				</Heading>
 
-				<Input mb={4} placeholder={"Qual o código do bolão?"} />
-				<Button title={"Buscar Bolão"} />
+				<Input
+					mb={4}
+					placeholder={"Qual o código do bolão?"}
+					autoCapitalize={"characters"}
+					onChangeText={setCode}
+				/>
+				<Button
+					title={"Buscar Bolão"}
+					isLoading={isLoading}
+					onPress={handleJoinPoll}
+				/>
 			</VStack>
 		</VStack>
 	);
